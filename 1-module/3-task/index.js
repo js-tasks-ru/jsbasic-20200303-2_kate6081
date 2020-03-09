9988e46442f3a739fb0f6ad85237bfc04879b48a
Ekaterina Maksimova <53267139+lanasaturn@users.noreply.github.com>
2020-03-09 22:50:59 +0300

    1 модуль. Задача №3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,11 @@
  * @param {string} str
  * @returns {string}
  */
+
 function ucFirst(str) {
+    if (!str) {
+        return str
+    } else {
+        return str[0].toUpperCase()+str.slice(1)
+    }
 }
