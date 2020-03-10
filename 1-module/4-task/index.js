a3cd82882193c6a921dde2c99d77a893931bb02c
Ekaterina Maksimova <53267139+lanasaturn@users.noreply.github.com>
2020-03-10 10:08:49 +0300

    1 модуль. Задача 4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-
+    str = str.toLowerCase();
+    if (str.includes("1xbet") || str.includes("xxx")){
+        return true;
+    } else {
+        return false
+    }
 }
