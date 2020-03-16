ff2d14a76b955d996d8ed2471e379f369fc620a1
Ekaterina Maksimova <53267139+lanasaturn@users.noreply.github.com>
2020-03-16 13:00:52 +0300

    Занятие 2: Объекты. Задача 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,14 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
-}
+    let sum = 0;
+    for (let symb of Object.values(salaries)) {
+        if (symb === 0 || (symb / symb === 1 )){
+            sum += symb; 
+        }
+        
+      }
+    
+      return sum;
+    }
+    
