de8f31d4aff4c6e87c75db9ecd024f49021e5fe0
Ekaterina Maksimova <53267139+lanasaturn@users.noreply.github.com>
2020-03-29 18:44:09 +0300

    3 модуль. 1 задача

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
 function showSalary(data, age) {
-}
+    return data
+    .filter(item => item.age <= age)
+      .map(item => `${item.name}, ${item.balance}`)
+      .join('\n');
+  }
+
+
+
